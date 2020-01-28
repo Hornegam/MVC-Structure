6a7563627f15264711dbde515d71dde3a208e0cc
Edson Junior <55529750+EDnotSheeran@users.noreply.github.com>
2020-01-28 00:10:49 -0300

    Update Connection.php

--- Lib/DataBase/Connection.php
+++ Lib/DataBase/Connection.php
@@ -6,7 +6,7 @@ abstract class Connection{
 
     public static function getConn(){
         if(self::$conn == null){
-            self::$conn = new PDO('mysql: host=localhost; dbname=Banco_Postagens;', 'root', '');
+            self::$conn = new PDO('mysql: host=localhost; dbname=Banco;', 'root', '');
         }
         return self::$conn;
     }
