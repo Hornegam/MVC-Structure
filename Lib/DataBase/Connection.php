4fcbd6604a2523a72803f05c25964739a54debda
EDnotSheeran <juninho.tab3@gmail.com>
2020-02-05 15:50:56 -0300

    add string de cofiguracao substituida por um aquivo json

--- Lib/DataBase/Connection.php
+++ Lib/DataBase/Connection.php
@@ -4,9 +4,21 @@ abstract class Connection{
 
     private static $conn;
 
-    public static function getConn(){
+    public static function getConn($config = "Lib/DataBase/db.config.json"){
+        //pega os dados do arquivo
+        $config = file_get_contents($config);
+        //decodifica a string em json em um array ou objeto 
+        $config = json_decode($config);
+        var_dump($config);
+        $database = $config->{'database'};
+        $host = $config->{'host'};
+        $banco = $config->{'dbname'};
+        $usuario = $config->{'usuario'};
+        $senha = $config->{'senha'};
+
+        //impede de criar varias instancias de conexao
         if(self::$conn == null){
-            self::$conn = new PDO('mysql: host=localhost; dbname=Banco;', 'root', '');
+            self::$conn = new PDO($database.': host='.$host.'; dbname='.$banco.';', $usuario, $senha);
         }
         return self::$conn;
     }
